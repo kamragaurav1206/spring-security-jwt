237753d755056f73eff0b2dbad73de39f59e8f57
kamra.gaurav <gauravjava1206@gmail.com>
2023-04-01 14:24:40 +0530

    jwt authentication implementation

--- src/main/java/com/yash/config/SecurityConfig.java
+++ src/main/java/com/yash/config/SecurityConfig.java
@@ -1,37 +1,60 @@
 package com.yash.config;
 
+import com.yash.filter.JWTFilter;
 import com.yash.service.CustomUserDetailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    Logger logger  = LoggerFactory.getLogger(SecurityConfig.class);
+
+    @Autowired
+    JWTFilter jwtFilter;
     @Autowired
     private CustomUserDetailService customUserDetailService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        logger.info("protected void configure(HttpSecurity http)");
+        System.out.println("protected void configure(HttpSecurity http)");
+        http.csrf().disable().authorizeRequests().antMatchers("/test/authenticate").permitAll().anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        logger.info("protected void configure(AuthenticationManagerBuilder auth)");
+        System.out.println("protected void configure(AuthenticationManagerBuilder auth)");
         auth.userDetailsService(customUserDetailService);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
--- src/main/java/com/yash/controller/WelcomeController.java
+++ src/main/java/com/yash/controller/WelcomeController.java
@@ -1,17 +1,50 @@
 package com.yash.controller;
 
+import com.yash.entity.AuthRequest;
+import com.yash.service.CustomUserDetailService;
+import com.yash.util.JwtUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/test/")
+@RequestMapping("/test")
 public class WelcomeController {
+
+    Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    CustomUserDetailService customUserDetailService;
 
     @GetMapping("/welcome")
     public ResponseEntity<String> welcome(){
+        System.out.println("Helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
         return new ResponseEntity<>("Welcome!!", HttpStatus.OK);
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<String> generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+        logger.info("user trying to autheticate "+authRequest.getUserName());
+        System.out.println("user trying to autheticate " + authRequest.getUserName());
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUserName(),authRequest.getPassword()));
+        }catch (Exception exception){
+            throw new Exception("Invalid user name and password");
+        }
+        UserDetails userDetails = customUserDetailService.loadUserByUsername(authRequest.getUserName());
+        return new ResponseEntity<>(jwtUtil.generateToken(userDetails),HttpStatus.OK);
     }
 }
